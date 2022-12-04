c10e9fa1be5736fdef70b9e1dadbc340725eab03
Fernandojunqueira <fernandojunqueira12@gmail.com>
2022-12-04 09:04:26 -0300

    finalização da entrega

--- crud.sql
+++ crud.sql
@@ -4,31 +4,77 @@
 -- Criação
 
 -- 1)
-
-
-
+INSERT INTO
+  clientes(nome,lealdade)
+VALUES 
+ ('Georgia',0);
 -- 2)
-
-
-
+INSERT INTO pedidos
+    (status, cliente_id)
+VALUES
+	('Recebido',6);
 -- 3)
-
-
-
+INSERT INTO produtos_pedidos
+  (pedido_id, produto_id)
+VALUES
+	(6,1),
+	(6,2),
+	(6,6),
+	(6,8);
 -- Leitura
 
 -- 1)
-
-
-
+SELECT 
+	c.id ,
+	c.nome ,
+	c.lealdade ,
+	p.id,
+	p.status ,
+	p.cliente_id ,
+	pr.id,
+	pr.nome,
+	pr.tipo,
+	round(pr.preço::NUMERIC,2) ,
+	pr.pts_de_lealdade	
+FROM 
+clientes c 
+JOIN 
+produtos_pedidos pp ON c.id = pp.pedido_id 
+JOIN 
+pedidos p ON p.id = pp.pedido_id 
+JOIN 
+produtos pr ON pr.id = pp.produto_id
+WHERE
+c.nome = 'Georgia';
 -- Atualização
 
 -- 1)
-
+UPDATE 
+clientes 
+SET
+lealdade = (SELECT 
+ round(sum(pr.preço)::NUMERIC,2)  
+FROM 
+clientes c 
+JOIN 
+produtos_pedidos pp ON c.id = pp.pedido_id 
+JOIN 
+pedidos p ON p.id = pp.pedido_id 
+JOIN 
+produtos pr ON pr.id = pp.produto_id
+WHERE
+c.nome = 'Georgia'
+GROUP BY	
+c.nome )
+WHERE 
+id=6;
 
 
 -- Deleção
 
 -- 1)
 
-
+DELETE FROM 
+clientes 
+WHERE 
+nome = 'Marcelo';
--- selects.sql
+++ selects.sql
@@ -2,11 +2,70 @@
 -- Seleções de dados
 
 -- 1)
-
+SELECT 
+  pe.id ,
+  pe.status,
+  pe.cliente_id,
+  pr.id ,
+  pr.nome ,
+  pr.tipo ,
+  pr.preço ,
+  pr.pts_de_lealdade 	
+FROM 
+pedidos pe 
+FULL JOIN 
+produtos_pedidos pp ON pe.id = pp.pedido_id 
+FULL JOIN 
+produtos pr ON pp.produto_id = pr.id ;
 -- 2)
-
+SELECT 
+pe.id  	
+FROM 
+pedidos pe 
+FULL JOIN 
+produtos_pedidos pp ON pe.id = pp.pedido_id 
+FULL JOIN 
+produtos pr ON pp.produto_id = pr.id 
+WHERE
+pr.nome = 'Fritas';
 -- 3)
-
+SELECT 
+  c.nome gostam_de_fritas 
+FROM 
+pedidos pe 
+JOIN 
+produtos_pedidos pp ON pe.id = pp.pedido_id 
+JOIN 
+produtos pr ON pp.produto_id = pr.id 
+JOIN 
+clientes c ON c.id = pe.cliente_id 
+WHERE
+pr.nome = 'Fritas';
 -- 4)
-
+SELECT 
+ROUND(SUM(pr.preço)::numeric,2)
+FROM 
+pedidos pe 
+JOIN 
+produtos_pedidos pp ON pe.id = pp.pedido_id 
+JOIN 
+produtos pr ON pp.produto_id = pr.id 
+JOIN 
+clientes c ON c.id = pe.cliente_id 
+WHERE 
+c.nome = 'Laura';
 -- 5)
+SELECT 
+pr.nome , count(*)
+FROM 
+pedidos pe 
+JOIN 
+produtos_pedidos pp ON pe.id = pp.pedido_id 
+JOIN 
+produtos pr ON pp.produto_id = pr.id 
+JOIN 
+clientes c ON c.id = pe.cliente_id 
+GROUP BY
+pr.nome
+ORDER BY 
+pr.nome;
